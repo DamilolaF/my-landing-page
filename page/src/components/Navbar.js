e2778a13d20f15c71c5f4823320f0690723e4486
Damilola <dami_fagbo@yahoo.com>
2022-06-04 22:41:10 +0100

    change update

--- page/src/components/Navbar.js
+++ page/src/components/Navbar.js
@@ -1,11 +1,18 @@
-import Navbar from './components/Navbar';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=''>
-        <Navbar></Navbar>
-    </div>
+    
+        <nav><div className='logo'>Ostyler</div>
+         <ul className='app__navbar-links' >
+        <li className='p__opensans' ><a href="#home">Home</a></li>
+        <li className='p__opensans' ><a href="#about">About</a></li>
+        <li className='p__opensans' ><a href="#menu">Menu</a></li>
+        <li className='p__opensans' ><a href="#awards">Awards</a></li>
+        <li className='p__opensans' ><a href="#contact">Contact</a></li>
+      </ul></nav>
+    
   )
-}
+};
 
-export default Navbar
+export default Navbar;
