33ecddee3894914d1cb8af3fc39476fb91085556
Damilola <dami_fagbo@yahoo.com>
2022-06-08 23:53:20 +0100

    updates

--- page/src/components/Navbar.js
+++ page/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { FaBars } from 'react-icons/fa';
 import './Navbar.css';
+
+
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     
         <nav className="total-nav">
           <div className='desktop'>OSTYLER
-          </div>
-          <div className="navicon">
-          <FaBars className="icon"></FaBars> 
           </div>
          <ul className='app__navbar-links' >
               <li className='navbar-link' ><a href="#home">Home</a></li>
@@ -17,11 +18,29 @@ const Navbar = () => {
               <li className='navbar-link' ><a href="#awards">Awards</a></li>
               <li className='navbar-link' ><a href="#contact">Contact</a></li>
       </ul>
-        <div className="reserve">
-          <a href="http://geritch.com" className="p__opensans">Reservation</a>
+      <div className="reserve">
+          <a href="http://geritch.com" className="navbar-link">Reservation</a>
+        </div>
+      
+          <div className='app__navbar-smallscreen'>
+        <FaBars color='black' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+          <FaBars fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+          <ul className='app__navbar-smallscreen-links' >
+            <li className='navbar-link' ><a href="#home">Home</a></li>
+            <li className='navbar-link' ><a href="#about">About</a></li>
+            <li className='navbar-link' ><a href="#menu">Menu</a></li>
+            <li className='navbar-link' ><a href="#awards">Awards</a></li>
+            <li className='navbar-link' ><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        )}
+
         </div>
       </nav>
-    
+
   )
 };
 
