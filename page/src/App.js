b12a7ca82a5091054c9bec720f3e76fa990db412
Damilola <dami_fagbo@yahoo.com>
2022-06-10 23:47:05 +0100

    commit update

--- page/src/App.js
+++ page/src/App.js
@@ -2,19 +2,17 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Header from './components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
-import SubHeading from './components/SubHeading';
-
+//import SubHeading from './components/SubHeading';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-   <Navbar></Navbar>
+      <div className="App">
+  <Navbar></Navbar>
    <Header></Header>
-  <SubHeading></SubHeading> 
-    </div>
+  </div>
     </Router>
   );
 }
--- page/src/components/Header.js
+++ page/src/components/Header.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import Navbar from './Navbar';
-import SubHeading  from  './SubHeading';
-//import { images } from './src/images';
-import './Header.css'
+//import { images } from './src/images/';
+import './Header.css';
 
 const Header = () => {
   return (
     <div className='app__header app__wrapper section__padding' id='home' >
+    <Navbar></Navbar>
     
     <div className='app__wrapper_info' >
-      <SubHeading title='Chase the new flavour' />
       <h1 className='app__header-h1' >Flavors for royalty</h1>
-      <p className='p__opensans' style={{ margin: '2rem 0' }} >
+      <p className='navbar-link' style={{ margin: '2rem 0' }} >
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim neque sit nisi amet dicta in, ea sapiente placeat earum error.
       </p>
       <button type='button' className='custom__button' >Explore Menu</button>
     </div>
 
     <div className='app__wrapper_img'>
-      {/* <img src={images.header} alt="header-img" className='app__header-img' /> */}
+   {/* <img src={images.header} alt="header-img" className='app__header-img' /> */}
     </div>
   </div>
   )
