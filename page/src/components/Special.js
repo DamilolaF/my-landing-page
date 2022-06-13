c303293fdfdd92f068f130493242fdc09c642d37
Damilola <dami_fagbo@yahoo.com>
2022-06-14 00:50:22 +0100

    update

--- page/src/components/Special.js
+++ page/src/components/Special.js
@@ -4,8 +4,21 @@ import special from '../images/special-pizza.jpg';
 
 const Special = () => {
   return (
-    
-  )
-}
+
+      <div className="specialContainer">
+         <h2>dd</h2>
+        <div className="specialContent">
+            <div className="textContent">
+          <p>
+          You'll be transported to Italy with our signature pepperoni pizza.
+         A perfect blend of fresh, quality ingredients and delicious, tasty flavors will
+          make you want to order another pizza.
+              </p>  
+        </div>
+      </div>
+    <button className='btn'>Order Now</button>
+    </div>
+    );
+};
 
 export default Special;
