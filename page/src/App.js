05f070aaaf6d4d8c262172897bc2fe4fdccbf3cf
Damilola <dami_fagbo@yahoo.com>
2022-06-15 15:08:57 +0100

    update

--- page/src/App.js
+++ page/src/App.js
@@ -7,6 +7,10 @@ import Header from './components/Header';
 import Cakes from './components/Cakes';
 import Foods from './components/Foods'
 import Special from './components/Special';
+import Footer from './components/Footer';
+import Chefs from './components/Chefs';
+
+
 function App() {
   return (
     <Router>
@@ -16,9 +20,11 @@ function App() {
    <Foods id="foods"></Foods>
    <Cakes id="dessert"></Cakes>
    <Special></Special>
+   <Chefs id="chef"></Chefs>
+   <Footer></Footer>
   </div>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ page/src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import spoon from '../images/spoon.png';
+import cutlery from '../images/cutlery.png';
+
+
+const Footer = () => ( 
+ 
+<div className="app__footer">
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">234 Adekunle Ajasin Drive, Surulere</p>
+        <p className="p__opensans">+234 21 763 2289</p>
+        <p className="p__opensans">+234 21 983 4631</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={cutlery} alt="footer-logo" className='logo' />
+        <p className="p__opensans">You deserve a meal that's as delicious as it is memorable. And we're here to help you make that happen.</p>
+        <img src={spoon} alt="footer-spoon" className='spoon__img' style={{marginTop: 15}} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday - Friday</p>
+        <p className="p__opensans">09:00am - 09:00pm</p>
+        
+        <p className="p__opensans">Saturday - Sunday</p>
+        <p className="p__opensans">09:00am - 00:00am</p>
+      </div>
+    
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Ostyler. All Rights Reserved</p>
+    </div>
+  </div>
+</div>
+);
+  
+export default Footer;
