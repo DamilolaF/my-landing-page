ef392303ae1b03b8734ea615d24adfb7d3b792c8
Damilola <dami_fagbo@yahoo.com>
2022-06-19 23:36:27 +0100

    commit updated

--- page/src/components/Foods.js
+++ page/src/components/Foods.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Foods.css';
+//import './Foods.css';
 import { foods } from '../data/foods';
 //import Cakes from './Cakes';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { settings } from '../data/foods';
 
 const Foods = () => {
     //<Cakes></Cakes>
@@ -13,9 +14,10 @@ const Foods = () => {
         <div className="foodContainer" id="menu">
             <h1 className="foodHead">Popular Meal menu </h1>
             <div className="foodContent" >
-              
+            
               {foods.map((food, index)=> (
-                  
+                 
+                 <Slider {...settings}>
 
                   <div className="foodCard" key={index}>
                     <img src={food.img} alt={food.alt} className="foodImg" />
@@ -25,8 +27,9 @@ const Foods = () => {
                     <button className='foodBtn'>Select</button>
                     </div>
                   </div>
-                  
-              ))}
+                 </Slider>
+              ))};    
+              
             </div>
         </div>   
         );
--- page/src/data/foods.js
+++ page/src/data/foods.js
@@ -42,3 +42,40 @@ export const foods = [
 },
 ];
 
+
+export const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplaySpeed: 500,
+    cssEase: 'linear',
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
